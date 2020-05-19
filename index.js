945beb2914588bd7381c48fba6095e1e8b0c503c
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-05-19 03:19:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+} );
+
+function updateDOM() {
+    document.getElementById('text')
+    .innerHTML = "This is really cool!";
+}
+      document.addEventListener("DOMContentLoaded", function() {
+      console.log("The DOM has loaded");
+    });
+     
+    console.log(
+      "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+    );
+    function myFunction() {
+        document.getElementById("text").innerHTML = "Hello World";
+      }
